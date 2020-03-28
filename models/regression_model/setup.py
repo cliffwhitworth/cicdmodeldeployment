e47cf2cae99a481c934b4196ca6f1af69fad3287
cliffwhitworth <emailme@cliffordwhitworth.com>
2020-03-28 11:24:56 -0500

    gemfury support

--- models/regression_model/setup.py
+++ models/regression_model/setup.py
@@ -11,9 +11,9 @@ from setuptools import find_packages, setup
 # Package meta-data.
 NAME = 'regression_model'
 DESCRIPTION = 'Train and deploy regression model.'
-URL = 'your github project'
-EMAIL = 'your_email@email.com'
-AUTHOR = 'Your name'
+URL = 'https://github.com/cliffwhitworth/cicdmodeldeployment'
+EMAIL = 'cliff.whitworth@gmail.com'
+AUTHOR = 'Cliff Whitworth'
 REQUIRES_PYTHON = '>=3.6.0'
 
 
